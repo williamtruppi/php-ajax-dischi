ae39f347118cc35f610bc77c24716783fdb069f2
William Truppi <williamt@hotmail.it>
2021-02-02 14:32:29 +0100

    struttura navbar realizzata

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="dist/css/style.css">
+</head>
+
+<body>
+
+  <!-- apertura navbar -->
+  <nav>
+    <div class="navbar container d-flex">
+      <img src="dist/img/logo.png" alt="">
+    </div>
+  </nav>
+  <!-- chiusura navbar -->
+
+  <!-- apertura main -->
+  <main>
+
+    <!-- apertura disk_box -->
+    <div class="disk_box">
+
+      <!-- apertura disk_container -->
+      <div class="disk_container">
+
+      </div>
+      <!-- chiusura disk_container -->
+
+    </div>
+    <!-- chiusura disk_box -->
+
+  </main>
+  <!-- chiusura main -->
+
+  <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+  <script src="dist/js/main.js"></script>
+</body>
+
+</html>
